ddb8bfa78f1afec5264e02b41b8600deca0798fa
rodrdan <daniel.dan.rodr@gmail.com>
2024-05-08 17:25:06 +0200

    3 different options for properties configurations added

--- microservices-accounts/accounts/src/main/java/cz/rodr/accounts/controller/AccountsController.java
+++ microservices-accounts/accounts/src/main/java/cz/rodr/accounts/controller/AccountsController.java
@@ -1,6 +1,7 @@
 package cz.rodr.accounts.controller;
 
 import cz.rodr.accounts.constants.AccountsConstants;
+import cz.rodr.accounts.dto.AccountsContactInfoDto;
 import cz.rodr.accounts.dto.CustomerDto;
 import cz.rodr.accounts.dto.ErrorResponseDto;
 import cz.rodr.accounts.dto.ResponseDto;
@@ -14,11 +15,15 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.*;
+
+import java.util.Map;
 
 @Tag(
         name = "CRUD REST APIs for Accounts in EazyBank",
@@ -26,11 +31,26 @@ import org.springframework.web.bind.annotation.*;
 )
 @RestController
 @RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
-@AllArgsConstructor
+// @AllArgsConstructor removed so we can use custom constructor without the @Value field
 @Validated
 public class AccountsController {
 
-    private IAccountsService iAccountsService;
+    private final IAccountsService iAccountsService;
+
+    public AccountsController(IAccountsService iAccountsService) {
+        this.iAccountsService = iAccountsService;
+    }
+
+    // reading properties method n. 1
+    @Value("${build.version}")
+    private String buildVersion;
+
+    // reading properties method n. 2
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private AccountsContactInfoDto accountsContactInfoDto;
 
     @Operation(
             summary = "Create account REST API",
@@ -130,5 +150,80 @@ public class AccountsController {
                     .status(HttpStatus.EXPECTATION_FAILED)
                     .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_DELETE));
         }
+    }
+
+    // reading properties using @Value annotated field (from application.yml)
+    @Operation(
+            summary = "Get build information REST API",
+            description = "REST API to get build information that is deployed into accounts microservice"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )}
+    )
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(buildVersion);
+    }
+
+    // reading properties using Environment interface field (from local environment variables)
+    @Operation(
+            summary = "Get Java version REST API",
+            description = "REST API to get Java version that is installed into accounts microservice"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )}
+    )
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    // reading properties using @ConfigurationProperties annotation (from application.yml + AccountsContactInfoDto class)
+    @Operation(
+            summary = "Get contact info REST API",
+            description = "REST API to get contact on accounts microservice developer"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )}
+    )
+    @GetMapping("/contact-info")
+    public ResponseEntity<Map<String, String>> getContactInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(accountsContactInfoDto.contactDetails());
     }
 }
